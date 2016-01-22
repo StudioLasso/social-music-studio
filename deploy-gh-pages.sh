552a031fd89a43bd65081294fffa3b1f9f287edc
Paul Musso <cpopul@gmail.com>
2016-01-23 00:14:06 +0100

    Update deploy-gh-pages.sh

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,4 +6,4 @@ git config user.name "PaulMusso"
 git config user.email "paul.musso@orange.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
